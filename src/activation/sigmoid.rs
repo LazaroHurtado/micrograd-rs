f9a742bf020f4e0523c18c5c10e0dadab98baa45
Pochetes <robertomiguel2001@gmail.com>
2023-02-07 07:55:13 -0500

    review changes

--- src/activation/sigmoid.rs
+++ src/activation/sigmoid.rs
@@ -3,7 +3,6 @@ use crate::value::Value;
 
 impl Activation {
     pub fn sigmoid(&self, value: Value) -> Value {
-        let one = Value::new(1.0);
-        one / (one + (-&value).exp())
+        Value::one() / (Value::one() + (-&value).exp())
     }
 }
