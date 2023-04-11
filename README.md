95285399ce3cf175afc3a59f4d548c970a5de665
Demba3 <79030319+Demba3@users.noreply.github.com>
2023-04-11 01:38:33 -0400

    Added instructions on how to load Pytorch models

--- README.md
+++ README.md
@@ -32,3 +32,28 @@ Any type of contribution is welcome as long as it adds value! i.e
   - ~~Padding Support~~
     - Padding Type
   - ~~Dilation Support~~
+  
+  ## Loading a model from Pytorch
+We need the convert_state_dict() function to convert PyTorch tensors to lists because micrograd_rs can't unpickle PyTorch tensors. This conversion will allow micrograd_rs to load pytorch models without any issues.
+```python
+# we need this import to serialize the model in a compatible format 
+import pickle
+
+# changes PyTorch generated state dict to micrograd state dict
+def convert_state_dict(state_dict):
+    new_state_dict = {}
+    for name, tensor in state_dict.items():
+        new_state_dict[name] = tensor.float().flatten().tolist()
+    return new_state_dict
+
+new_state_dict = convert_state_dict(model.state_dict())
+
+# stores new state dict
+with open(path, "wb") as f:
+    pickle.dump(new_state_dict, f)
+```
+  
+```rust
+// To load model in rust
+model.load_state_dict(path);
+```
--- tests/layers/model.rs
+++ tests/layers/model.rs
@@ -1,9 +1,11 @@
 extern crate micrograd_rs;
+use indexmap::IndexMap;
 use micrograd_rs::activations as Activation;
 use micrograd_rs::pooling::AvgPool;
 use micrograd_rs::prelude::*;
 use micrograd_rs::Conv2D;
 use micrograd_rs::{Layer, Linear, Model, Sequential};
+use serde_pickle::{de, DeOptions};
 use std::fs;
 
 #[test]
@@ -62,8 +64,8 @@ fn valid_save_and_load_state_dict_for_convolutional_model() {
                 name,
                 in_channels,
                 out_channels,
-                padding,
                 (2, 2),
+                padding,
                 (1, 1),
                 (1, 1),
             ),
@@ -79,8 +81,8 @@ fn valid_save_and_load_state_dict_for_convolutional_model() {
                 name,
                 in_channels,
                 out_channels,
-                padding,
                 (2, 2),
+                padding,
                 (1, 1),
                 (1, 1),
             ),
@@ -101,4 +103,62 @@ fn valid_save_and_load_state_dict_for_convolutional_model() {
         fs::remove_file(path).is_ok(),
         "File \"convolutional.pickle\" could not be removed."
     );
+}
+
+#[test]
+fn valid_load_of_pytorch_convolutional_model() {
+    let path = "tests/layers/Pytorch_ConvolutionalModel.pt";
+    let file = fs::File::open(path).unwrap();
+    let convo_state_dict: IndexMap<String, Vec<f64>> =
+        de::from_reader(file, DeOptions::new()).unwrap();
+
+    let name = "conv2d";
+    let padding = (1, 2);
+    let (in_channels, out_channels) = (3, 1);
+
+    let pool_size = (2, 2);
+    let pool_stride = (2, 1);
+
+    let mut convo_model = sequential!(
+        Ix4,
+        [
+            Conv2D::new(
+                name,
+                in_channels,
+                out_channels,
+                (2, 2),
+                padding,
+                (1, 1),
+                (1, 1),
+            ),
+            AvgPool::new(pool_size, pool_stride, (0, 0), (1, 1)),
+            Activation::Sigmoid
+        ]
+    );
+    convo_model.load_state_dict(path);
+
+    assert_eq!(convo_state_dict, convo_model.state_dict());
+}
+
+#[test]
+fn valid_load_of_pytorch_linear_model() {
+    let path = "tests/layers/Pytorch_LinearModel.pt";
+    let file = fs::File::open(path).unwrap();
+    let linear_state_dict: IndexMap<String, Vec<f64>> =
+        de::from_reader(file, DeOptions::new()).unwrap();
+
+    let mut linear_model = sequential!(
+        Ix1,
+        [
+            Linear::new("fc1", 15, 20),
+            Activation::Tanh,
+            Linear::new("fc2", 20, 10),
+            Activation::Tanh,
+            Linear::new("fc3", 10, 1),
+            Activation::Tanh
+        ]
+    );
+    linear_model.load_state_dict(path);
+
+    assert_eq!(linear_state_dict, linear_model.state_dict());
 }
